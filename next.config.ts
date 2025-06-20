99c2e7ea606ac8c6aa1057be3601e108c151dc2c
Saeed Moqadam <saeed.moqadam@gmail.com>
2025-06-20 14:15:34 +0200

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/ephemeral-tools' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ephemeral-tools' : '',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
